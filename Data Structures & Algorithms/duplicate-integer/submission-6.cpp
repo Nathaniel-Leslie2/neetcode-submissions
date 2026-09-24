eb0807a6e55a2953b32ce7f05f2a14b265931c3b
Nathaniel Leslie <64575673+Nathaniel-Leslie2@users.noreply.github.com>
2026-09-24 15:56:24 -0600

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for (auto n : nums)
+        {
+            if (seen.contains(n)) return true;
+            seen.insert(n);
+        }
+
+        return false;
+    }
+};
